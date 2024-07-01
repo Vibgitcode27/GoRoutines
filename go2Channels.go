e1d0feacba5308dff729da7f3f98acc47dd4899d
vibhor <vibgitcode27@gmail.com>
2024-07-01 17:24:50 +0530

    use of go channels and deadlock

--- go2Channels.go
+++ go2Channels.go
@@ -5,16 +5,21 @@ import (
 	"time"
 )
 
-func attack2(target string) {
+func attack2(target string, ensure chan bool) {
 	time.Sleep(1 * time.Second)
 	fmt.Println(target)
+	ensure <- true
 }
 
 func channelsImplementation() {
 	// Create a channel
 	ninja := "Vibhor"
+	channel := make(chan bool)
 
-	go attack2(ninja)
-
-	time.Sleep(2 * time.Second)
+	go attack2(ninja, channel)
+	channel <- false // Will lead to dealock
+	fmt.Println("Waiting for the goroutine to finish")
+	for !<-channel {
+	}
+	// Wait for the goroutine to finish
 }
